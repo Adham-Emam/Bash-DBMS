2994289654e8be50397496a2ed607fca1f03d633
Adham Emam <mrado372002@gmail.com>
2025-09-29 01:10:37 +0300

    add renameDB functionality

---
+++ functions/renameDB.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function renameDB {
+    clear
+    if [[ ! -d ./databases/$USERNAME ]]; then
+        echo "No databases found, please create one first."
+    else 
+        cd ./databases/$USERNAME
+        echo "Available databases:"
+        ls -d */ | sed 's:/$::'
+
+
+        while true; do
+            read -p "enter the name of the database you want to rename: " dbName
+            if [[ -d $dbName ]]; then
+                break
+            else 
+                echo "database not found."
+                continue
+            fi
+        done 
+
+        while true; do
+            read -p "enter the new name of the database: " newName
+
+            validateDBName $newName
+            if [[ $? -eq 0 ]]; then
+                break
+            fi
+        done
+
+        mv "$dbName" "$newName"
+        cd ../..
+        clear
+        echo "database renamed successfully"
+    fi
+}
