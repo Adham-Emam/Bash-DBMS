ceadb6fcde911212b2b64a958f3c22a4d7c3ab7f
youssef-M-hassan <youssef.m.hassan56@gmail.com>
2025-09-30 02:30:47 +0300

    validation for table name and column name

---
+++ functions/validateColumnName.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+function validateColumnName {
+	local column_Name="$1"
+	if [[ -z "$column_Name" ]]; then
+		echo "ERROR-- column name cannot be empty"
+		return 1
+	fi
+	if [[ "$column_Name" == *" "* ]]; then
+		echo "ERROR-- Column name cannot contain spaces"
+		return 1
+	fi
+	if [[ "$column_Name" =~ [^a-zA-Z0-9_] ]]; then
+		echo "ERROR--Column name cannot contain specialcharacters other than underscore"
+		return 0
+	fi
+	if [[ "$column_Name" =~ ^[0-9] ]]; then 
+		echo "column name cannot start witha number"
+		return 0
+	fi
+	if [[ $columnname == "exit" ]]; then
+		rm -f "$TABLE_PATH" "$META_PATH"
+		TablesMainMenu
+	fi
+	return 0
+}
